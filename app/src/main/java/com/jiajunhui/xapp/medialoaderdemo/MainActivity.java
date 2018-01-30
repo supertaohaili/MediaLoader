c71a16bd0fd23bb732914ae36c5679f18e607a89
陶海理 <15220055862@163.com>
2018-01-30 14:09:56 +0800

    1.0.0

--- app/src/main/java/com/jiajunhui/xapp/medialoaderdemo/MainActivity.java
+++ app/src/main/java/com/jiajunhui/xapp/medialoaderdemo/MainActivity.java
@@ -161,6 +161,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     private void loadVideos() {
         MediaLoader.getLoader().loadVideos(this, new OnVideoLoaderCallBack() {
             @Override
